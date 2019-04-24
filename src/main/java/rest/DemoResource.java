7ebc048a7babd422690b2ae2fe77e8bc167dbf9d
Mads Wulff Nielsen <mwn@Madss-MacBook-Pro.local>
2019-04-24 14:06:34 +0200

    backend done

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -1,8 +1,17 @@
 package rest;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import entity.User;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManager;
 import javax.ws.rs.core.Context;
@@ -20,48 +29,83 @@ import utils.PuSelector;
 @Path("info")
 public class DemoResource {
 
-  @Context
-  private UriInfo context;
+    @Context
+    private UriInfo context;
 
-  @Context
-  SecurityContext securityContext;
+    @Context
+    SecurityContext securityContext;
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  public String getInfoForAll() {
-    return "{\"msg\":\"Hello anonymous\"}";
-  }
-
-  //Just to verify if the database is setup
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  @Path("all")
-  public String allUsers() {
-    EntityManager em = PuSelector.getEntityManagerFactory("pu").createEntityManager();
-    try{
-      List<User> users = em.createQuery("select user from User user").getResultList();
-      return "["+users.size()+"]";
-    } finally {
-      em.close();
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getInfoForAll() {
+        return "{\"msg\":\"Hello anonymous\"}";
     }
- 
-  }
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  @Path("user")
-  @RolesAllowed("user")
-  public String getFromUser() {
-    String thisuser = securityContext.getUserPrincipal().getName();
-    return "{\"msg\": \"Hello to User: " + thisuser + "\"}";
-  }
+    //Just to verify if the database is setup
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("all")
+    public String allUsers() {
+        EntityManager em = PuSelector.getEntityManagerFactory("pu").createEntityManager();
+        try {
+            List<User> users = em.createQuery("select user from User user").getResultList();
+            return "[" + users.size() + "]";
+        } finally {
+            em.close();
+        }
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  @Path("admin")
-  @RolesAllowed("admin")
-  public String getFromAdmin() {
-    String thisuser = securityContext.getUserPrincipal().getName();
-    return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
-  }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("user")
+    @RolesAllowed("user")
+    public String getFromUser() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Hello to User: " + thisuser + "\"}";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("admin")
+    @RolesAllowed("admin")
+    public String getFromAdmin() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("apis")
+    public String getFromAPIs() {
+        return getData();
+    }
+
+    public String getSwappiData(int id) throws MalformedURLException, IOException {
+        URL url = new URL("https://swapi.co/api/people/" + id);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+        con.setRequestProperty("User-Agent", "server");
+        Scanner scan = new Scanner(con.getInputStream());
+        String jsonStr = null;
+        if (scan.hasNext()) {
+            jsonStr = scan.nextLine();
+        }
+        scan.close();
+        return jsonStr;
+    }
+
+    public String getData() {
+        try {
+            String a = getSwappiData(1);
+            String b = getSwappiData(2);
+            String c = getSwappiData(3);
+            String d = getSwappiData(4);
+            String e = getSwappiData(5);
+            return "{\"persons\": [ " + a + ", " + b + ", " + c + ", " + d + ", " + e + "]}";
+        } catch (IOException ex) {
+            return null;
+        }
+    }
 }
